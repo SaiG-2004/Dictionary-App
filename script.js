5489b41c64423c4c0726ed406c1fc7376dc35d44
Sai Gayatri Khuntia <saigayatrikhuntia44@gmail.com>
2024-04-07 15:29:45 +0530

    final commit

--- script.js
+++ script.js
@@ -2,19 +2,34 @@ const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 
 const searchBtn = document.querySelector("#search-btn");
 const word = document.querySelector("#word");
+const spinner = document.querySelector(".spinner");
+const error = document.querySelector(".error-boundary");
 
 const searchWord = async () => {
+
+   spinner.style.display = "block";
+  error.style.display = "none";
   document.querySelector(".result").innerHTML = "";
 
-  const response = await fetch(url + word.value);
-  const data = await response.json();
-  console.log(data);
+  
+  
+      const response = await fetch(url + word.value);
+      const data = await response.json();
+  
+      if(data.title){
+        spinner.style.display = "none";
+        error.style.display = "block";
+      }
+     else{
 
-  const meanings = data[0].meanings;
+      const meanings = data[0].meanings;
 
-  meanings.forEach((e) => {
-    createBox(e);
-  });
+      meanings.forEach((e) => {
+        createBox(e);
+      });
+    
+       spinner.style.display = "none";
+  
 
   function createBox(e) {
     let synonymsData;
@@ -38,29 +53,29 @@ const searchWord = async () => {
     let template = ` 
        <div class="card my-2" style="width:90%;">
          <div class="card-body">
-           <h6 class="card-title">${data[0].word}</h6>
+           <h6 class="card-title">${data[0]?.word}</h6>
            <h6 class="card-subtitle mb-2 text-muted"> ${
-             data[0].phonetics[2].text
-               ? data[0].phonetics[2].text
-               : data[0].phonetics[0].text
+             data[0]?.phonetics[2]?.text
+              ? data[0]?.phonetics[2]?.text
+              : data[0]?.phonetics[0]?.text
            }</h6>
 
            <div class="my-3 bg-danger px-2 rounded" style="width: max-content;">
-             <p class="text-light m-2">${e.partOfSpeech}</p>  
+             <p class="text-light m-2">${e?.partOfSpeech}</p>  
            </div>
 
            <div class="alert alert-success" role="alert">
              <p class="alert-heading"><strong><i>Definition</i></strong></p>
              <p class="card-text">${
-               e.definitions.length
-                 ? e.definitions[0].definition
+               e?.definitions?.length
+                 ? e?.definitions[0]?.definition
                  : "No definition found"
              }</p>
              <hr/>
              <p class="alert-heading"><strong><i>Example</i></strong></p>
              <p class="mb-0">${
-               e.definitions[0].example
-                 ? e.definitions[0].example
+               e?.definitions[0]?.example
+                 ? e?.definitions[0]?.example
                  : "no example found"
              }</p>
            </div>
@@ -77,8 +92,10 @@ const searchWord = async () => {
            </div>
      
       `;
+    
     document.querySelector(".result").innerHTML += template;
   }
+     }
 };
 
 searchBtn.addEventListener("click", searchWord);
